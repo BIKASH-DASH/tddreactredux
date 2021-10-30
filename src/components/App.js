836e41739b819b8144aaa2896ae6aaf0b878d1b2
bikash <vikashdash004@gmail.com>
2021-10-30 16:13:51 +0530

    init jest

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h2>loot check</h2>
+            </div>
+        )
+    }
+}
